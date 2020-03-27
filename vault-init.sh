bf451f4296ba033a0ef91ab1a2e63d6afc818d20
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-03-27 16:54:20 -0400

    Update with namespace

--- vault-init.sh
+++ vault-init.sh
@@ -6,4 +6,4 @@ if [ $? -eq 2 ]; then
   kubectl exec -i -n $KUBE_NAMESPACE vault-0 -- vault operator init | grep "Initial Root Token"
 fi
 
-kubectl get service vault-ui -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+kubectl get service -n $KUBE_NAMESPACE vault-ui -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
